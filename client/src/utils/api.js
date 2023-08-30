0b53f6d62bf884878ec3e14a8f833d7c3c2496a2
MaizNadeem <maiznadeem616@gmail.com>
2023-08-31 03:36:06 +0500

    Updated API again

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "great-read-server.vercel.app"
+    baseURL: "https://great-read.uc.r.appspot.com"
 });
 
 export async function getBooks(offset, limit, categories) {
