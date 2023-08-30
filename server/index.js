b3b9a1a03fd2673abd4ae94a26a096ac617cea56
MaizNadeem <maiznadeem616@gmail.com>
2023-08-31 03:27:03 +0500

    Created app.yaml

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 const dbURL = process.env.MONGO_DB_URL;
 const secretKey = process.env.SESSION_SECRET;
 
