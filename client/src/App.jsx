4b2ea321e34e203d33c87d00c5c07563bcdbc317
MaizNadeem <maiznadeem616@gmail.com>
2023-09-10 20:57:34 +0500

    Redirecting HTTP to HTTPS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,19 +11,23 @@ import ConnectDots from './pages/ConnectDots';
 const queryClient = new QueryClient();
 
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter basename="/great-read.com">
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/readingreflections" element={<ConnectDots />} />
-          </Route>
-          <Route path="*" component={<div>404 NOT FOUND</div>} />
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
-  );
+
+    if (window.location.protocol === 'http:') {
+        window.location.href = `https://${window.location.hostname}${window.location.pathname}${window.location.search}`;
+    }
+    return (
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter basename="/great-read.com">
+                <Routes>
+                <Route element={<Layout />}>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/readingreflections" element={<ConnectDots />} />
+                </Route>
+                <Route path="*" component={<div>404 NOT FOUND</div>} />
+                </Routes>
+            </BrowserRouter>
+        </QueryClientProvider>
+    );
 }
 
 export default App;
