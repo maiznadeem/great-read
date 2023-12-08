2585ff1a4e8ca92765c0bc9db739cfe7bf89908c
MaizNadeem <maiznadeem616@gmail.com>
2023-12-09 00:33:07 +0500

    Select Product Complete

---
+++ client/src/components/SelectProduct.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import MuiToggleButton from "@mui/material/ToggleButton";
+import { styled } from "@mui/material/styles";
+
+const ToggleButton = styled(MuiToggleButton)({
+    width: "100%",
+    color: 'white',
+    backgroundColor: '#fac35c',
+    fontSize: 16,
+    textTransform: 'none',
+    border: "1px solid #FFA500",
+    borderRadius: 15,
+    "&.Mui-selected, &.Mui-selected:hover": {
+        color: "white",
+        backgroundColor: '#FFA500',
+    },
+    "&:not(.Mui-selected):hover": {
+        color: 'white',
+        backgroundColor: '#FFA500',
+    },
+    "&:hover": {
+        backgroundColor: '#FFA500',
+    },
+});
+
+const SelectProduct = ({ selectedButton, handleButtonClick }) => {
+
+    return (
+        <div className='bg-footer w-full flex flex-col justify-center items-center p-8 gap-4 rounded-xl shadow-lg'>
+            <p className='manrope-regular text-black text-xl text-center max-w-[500px]'>Access <span className='text-primary'>5-8 key word-by-word extracted</span> insights, ideas or concepts from any book.</p>
+            <div className='w-full flex gap-2 sm:gap-4 flex-col sm:flex-row manrope-semibold text-md sm:text-lg max-w-[200px] sm:max-w-none'>
+                <ToggleButton
+                    selected={selectedButton === 1}
+                    onClick={() => handleButtonClick(1)}
+                    style={{
+                        backgroundColor: selectedButton == null ? '#FFA500' : ''
+                    }}
+                >
+                    Any 10 books from <br /> 1 category <br /> £29
+                </ToggleButton>
+                <ToggleButton
+                    selected={selectedButton === 2}
+                    onClick={() => handleButtonClick(2)}
+                    style={{
+                        backgroundColor: selectedButton == null ? '#FFA500' : ''
+                    }}
+                >
+                    Any 10 books from <br /> 1-3 categories <br /> £49
+                </ToggleButton>
+                <ToggleButton
+                    selected={selectedButton === 3}
+                    onClick={() => handleButtonClick(3)}
+                    style={{
+                        backgroundColor: selectedButton == null ? '#FFA500' : ''
+                    }}
+                >
+                    Any 30 books from <br /> any category <br /> £59
+                </ToggleButton>
+            </div>
+            <p className='manrope-regular text-black text-sm text-center max-w-[500px]'>Downloadable as: PDF and Word</p>
+        </div>
+    )
+}
+
+export default SelectProduct;
--- client/src/pages/Notes.jsx
+++ client/src/pages/Notes.jsx
@@ -1,65 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import { getRandomNotes } from '../utils/api';
-import NotesSlider from '../components/NotesSlider';
-import BookmarkCard from '../components/BookmarkCard';
-import DownloadNotes from '../components/DownloadNotes';
+import SelectProduct from '../components/SelectProduct';
+import Preview from '../components/Preview';
 
 const Notes = () => {
-    const [shuffledNotes, setShuffledNotes] = useState([]);
-    const [isShuffling, setIsShuffling] = useState(false);
-    const [currentSlide, setCurrentSlide] = useState(1);
-    const [resetFlip, setResetFlip] = useState(false);
 
-    const handleCurrentSlideChange = (newValue) => {
-        if (currentSlide == newValue)
-            return;
-        setIsShuffling(true);
-        setResetFlip((prev) => !prev);
-        setCurrentSlide(newValue);
+    const [selectedButton, setSelectedButton] = useState(null);
+
+    const handleButtonClick = (value) => {
+        setSelectedButton(value === selectedButton ? null : value);
     };
 
-    useEffect(() => {
-        const abortController = new AbortController();
-        getRandomNotes(currentSlide, {
-            signal: abortController.signal,
-        })
-        .then(async data => {
-            return data;
-        })
-        .then(
-            result => {
-                if (abortController.signal.aborted) {
-                    return;
-                }
-                setShuffledNotes(result);
-                setIsShuffling(false);
-            },
-            e => console.warn('fetch failure', e),
-        )
-        return () => {
-            abortController.abort();
-        };
-    }, [currentSlide]);
-
     return (
-        <section className='mt-6 sm:mt-12 sm:my-14 mx-4 sm:mx-8 min-h-[80vh]'>
+        <section className='mt-6 sm:mt-12 sm:my-14 mx-4 sm:mx-8 min-h-[100vh]'>
             <div className='flex flex-col gap-8 items-center justify-center w-full'>
-                <div className='flex flex-col gap-4 items-center w-full max-w-[600px]'>
-                    <p className='manrope-regular text-center text-xl text-black'>
-                        A collection of more than <span className='text-primaryDark'>10,000</span> interesting insights, ideas, and concepts from over 3,000 books.
-                    </p>
-                    <DownloadNotes />
+                <div className='flex flex-col gap-4 items-center w-full max-w-[700px]'>
+                    <SelectProduct selectedButton={selectedButton} handleButtonClick={handleButtonClick} />
                 </div>
-                <NotesSlider currentSlide={currentSlide} setCurrentSlide={handleCurrentSlideChange} isShuffling={isShuffling} />
-                <div className='w-full flex flex-wrap justify-center gap-12 p-12 pt-4 min-h-[600px]'>
-                    {shuffledNotes.length > 0 ? (
-                        shuffledNotes.map((note, index) => (
-                            <BookmarkCard key={index} note={note} isShuffling={isShuffling} resetFlip={resetFlip} />
-                        ))
-                    ) : (
-                        <p>No notes available.</p>
-                    )}
-                </div>
+                { !selectedButton &&
+                    <Preview />
+                }
             </div>
         </section>
     );
