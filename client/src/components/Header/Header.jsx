1775b66c4cf7593d144513ad2bb97034afd6ef08
M. Maiz Nadeem <maiznadeem616@gmail.com>
2024-09-05 00:40:18 +0500

    Reading List Menu Item Fix

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import Logo from '../../assets/logos/Logo.svg';
-import { useReadingList } from '../../context/ReadingListContext';
+import React, { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import Logo from "../../assets/logos/Logo.svg";
+import { useReadingList } from "../../context/ReadingListContext";
 
 const Header = () => {
     const [isHidden, setIsHidden] = useState(false);
@@ -14,7 +14,8 @@ const Header = () => {
     useEffect(() => {
         let lastScrollTop = 0;
         const handleScroll = () => {
-            const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            const currentScrollTop =
+                window.pageYOffset || document.documentElement.scrollTop;
             if (currentScrollTop > lastScrollTop) {
                 setIsHidden(true);
                 setIsAtTop(false);
@@ -25,36 +26,46 @@ const Header = () => {
             }
             lastScrollTop = currentScrollTop;
         };
-        window.addEventListener('scroll', handleScroll);
+        window.addEventListener("scroll", handleScroll);
         return () => {
-            window.removeEventListener('scroll', handleScroll);
+            window.removeEventListener("scroll", handleScroll);
         };
     }, []);
 
-    const buttonText = location.pathname === '/' ? 'Access Notes' : 'Homepage';
-    const buttonLink = location.pathname === '/' ? '/notes' : '/';
+    const buttonText = location.pathname === "/" ? "Access Notes" : "Homepage";
+    const buttonLink = location.pathname === "/" ? "/notes" : "/";
 
     const handleNotesButtonClicked = () => {
         history(buttonLink);
         setTimeout(() => {
-            window.scrollTo({ top: 0, behavior: 'smooth' });
-        }, 500)
+            window.scrollTo({ top: 0, behavior: "smooth" });
+        }, 500);
     };
 
     const handleReadingListClick = () => {
-        if (isReadingListActive) {
-            toggleReadingList();
+        if (window.location.pathname === "/notes") {
+            history("/");
+            if(!isReadingListActive) {
+                toggleReadingList();
+            }
         } else {
-            toggleReadingList();
-            setTimeout(() => {
-                const targetElement = document.getElementById("readinglistsection");
-                if (targetElement) {
-                    const targetHeight = targetElement.clientHeight;
-                    const windowHeight = window.innerHeight;
-                    const offset = targetElement.offsetTop - (windowHeight - targetHeight) / 2;
-                    window.scrollTo({ top: offset, behavior: "smooth" });
-                }
-            }, 50);
+            if (isReadingListActive) {
+                toggleReadingList();
+            } else {
+                toggleReadingList();
+                setTimeout(() => {
+                    const targetElement =
+                        document.getElementById("readinglistsection");
+                    if (targetElement) {
+                        const targetHeight = targetElement.clientHeight;
+                        const windowHeight = window.innerHeight;
+                        const offset =
+                            targetElement.offsetTop -
+                            (windowHeight - targetHeight) / 2;
+                        window.scrollTo({ top: offset, behavior: "smooth" });
+                    }
+                }, 50);
+            }
         }
     };
 
@@ -64,33 +75,51 @@ const Header = () => {
         }
         history("/");
         setTimeout(() => {
-            window.scrollTo({ top: 0, behavior: 'smooth' });
-        }, 500)
+            window.scrollTo({ top: 0, behavior: "smooth" });
+        }, 500);
     };
 
     return (
         <header
             className={`fixed top-0 left-0 w-full bg-backgroundPrimary ${
-                isHidden ? 'transform translate-y-[-80px]' : 'px-4 sm:px-8 py-4 border-primary'
-            } ${isAtTop ? '' : 'border-b-2 border-primary shadow-md'} transition-all duration-300 ease-in-out z-50`}
+                isHidden
+                    ? "transform translate-y-[-80px]"
+                    : "px-4 sm:px-8 py-4 border-primary"
+            } ${
+                isAtTop ? "" : "border-b-2 border-primary shadow-md"
+            } transition-all duration-300 ease-in-out z-50`}
         >
             {isAtTop && (
-                <div className="absolute w-[40%] sm:w-[30%] border-b-[3px] border-primary" style={{ bottom: '0' }}></div>
+                <div
+                    className="absolute w-[40%] sm:w-[30%] border-b-[3px] border-primary"
+                    style={{ bottom: "0" }}
+                ></div>
             )}
             <div className="flex justify-between items-center">
-                <button onClick={handleHomeClicked} className="text-white font-bold text-lg">
+                <button
+                    onClick={handleHomeClicked}
+                    className="text-white font-bold text-lg"
+                >
                     <img
                         src={Logo}
                         alt="Logo"
                         className={`transition-all duration-300 transform 
-                            ${isHidden ? 'h-0' : ''}
-                            ${!isHidden && !isAtTop ? 'h-16' : ''}
-                            ${!isHidden && isAtTop ? 'h-32 sm:h-32 max-h-96' : ''}
+                            ${isHidden ? "h-0" : ""}
+                            ${!isHidden && !isAtTop ? "h-16" : ""}
+                            ${
+                                !isHidden && isAtTop
+                                    ? "h-32 sm:h-32 max-h-96"
+                                    : ""
+                            }
                         `}
                     />
                 </button>
-                <div className='flex flex-col gap-1 sm:gap-2'>
-                    <div className={`flex gap-1 sm:gap-2 ${!isAtTop ? 'flex-row' : 'flex-col'} justify-center items-end sm:flex-row`}>
+                <div className="flex flex-col gap-1 sm:gap-2">
+                    <div
+                        className={`flex gap-1 sm:gap-2 ${
+                            !isAtTop ? "flex-row" : "flex-col"
+                        } justify-center items-end sm:flex-row`}
+                    >
                         <button
                             className="manrope-semibold bg-primary text-white text-[10px] py-[6px] px-[10px] rounded-md sm:px-6 sm:text-[14px] w-24 sm:w-36 sm:rounded-md shadow-lg hover:bg-primaryDark"
                             onClick={handleNotesButtonClicked}
