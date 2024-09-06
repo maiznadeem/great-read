1a03e111ca2c954ae68a503950fa7f1a1c3b7680
M. Maiz Nadeem <maiznadeem616@gmail.com>
2024-09-06 16:52:04 +0500

    Chnaged Popup background

--- client/src/components/ReadingList/ReadingListForm.jsx
+++ client/src/components/ReadingList/ReadingListForm.jsx
@@ -1,30 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import waves from '../../assets/backgrounds/waves.png';
-import linear from '../../assets/backgrounds/linear.png';
+import waves from "../../assets/backgrounds/waves.png";
 
-import enabled from '../../assets/buttons/enabled.svg';
-import disabled from '../../assets/buttons/disabled.svg';
+import enabled from "../../assets/buttons/enabled.svg";
+import disabled from "../../assets/buttons/disabled.svg";
 
-import { useReadingList } from '../../context/ReadingListContext';
+import { useReadingList } from "../../context/ReadingListContext";
 
-import Step1 from './StepperContent/Step1';
-import Step2 from './StepperContent/Step2';
-import Step3 from './StepperContent/Step3';
-import Step4 from './StepperContent/Step4';
+import Step1 from "./StepperContent/Step1";
+import Step2 from "./StepperContent/Step2";
+import Step3 from "./StepperContent/Step3";
+import Step4 from "./StepperContent/Step4";
 
 const ReadingListForm = () => {
-
-    const { setNameValue, setPeriodValue, setGoalValue, setReadingInfoValue, setSelectedCategoriesValue, setSelectionChoiceValue, toggleReadingList } = useReadingList();
+    const {
+        setNameValue,
+        setPeriodValue,
+        setGoalValue,
+        setReadingInfoValue,
+        setSelectedCategoriesValue,
+        setSelectionChoiceValue,
+        toggleReadingList,
+    } = useReadingList();
 
     const stepLength = 4;
 
     const [step, setStep] = useState(1);
-    const [name, setName] = useState('');
-    const [selectedTimePeriod, setSelectedTimePeriod] = useState('1 week');
+    const [name, setName] = useState("");
+    const [selectedTimePeriod, setSelectedTimePeriod] = useState("1 week");
     const [goal, setGoal] = useState(1);
-    const [select, setSelect] = useState('choose for me');
-    const [selectedCategories, setSelectedCategories]= useState([]);
+    const [select, setSelect] = useState("choose for me");
+    const [selectedCategories, setSelectedCategories] = useState([]);
     const nextStep = () => {
         if (step == 4) {
             handleChoose();
@@ -41,7 +47,7 @@ const ReadingListForm = () => {
         setGoalValue(goal);
         setSelectionChoiceValue(select);
         setReadingInfoValue(true);
-    }
+    };
     const handleSubmit = (e) => {
         e.preventDefault();
         if (step <= stepLength) {
@@ -52,11 +58,11 @@ const ReadingListForm = () => {
     const handleNameChange = (e) => {
         e.preventDefault();
         let newName = e.target.value;
-        const words = newName.split(' ');
+        const words = newName.split(" ");
         for (let i = 0; i < words.length; i++) {
             words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
         }
-        newName = words.join(' ');
+        newName = words.join(" ");
         if (newName.length > 40) {
             newName = newName.slice(0, 40);
         }
@@ -64,39 +70,98 @@ const ReadingListForm = () => {
     };
 
     return (
-        <div className='bg-footer py-3 px-6 sm:py-3 sm:px-8 rounded-xl w-full' style={{
-            background: `linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url(${linear}), url(${waves})`,
-            backgroundSize: 'cover, auto 100%, cover',
-            backgroundPosition: 'center, right, center',
-            backgroundRepeat: 'no-repeat',
-        }}>
-            <form onSubmit={handleSubmit}>
-                {step === 1 && <Step1 name={name} handleNameChange={handleNameChange} />}
-                {step === 2 && <Step2 name={name} selectedTimePeriod={selectedTimePeriod} setSelectedTimePeriod={setSelectedTimePeriod} />}
-                {step === 3 && <Step3 name={name} selectedTimePeriod={selectedTimePeriod} goal={goal} setGoal={setGoal} />}
-                {step === 4 && <Step4 name={name} goal={goal} select={select} setSelect={setSelect} />}
-                {/* {step === 5 && select === 'choose for me' && <CategoryStep name={name} selectedCategories={selectedCategories} setSelectedCategories={setSelectedCategories} handleCategoryClick={handleCategoryClick} />} */}
-                <div className={`button-container flex w-full ${ step > 1 ? 'justify-between' : 'justify-end' }`}>
-                    {step > 1 && (
-                        <button type="button" onClick={prevStep}>
-                            <img src={disabled} alt="Previous" className="h-8 w-8" />
-                        </button>
+        <div className="bg-footer w-full rounded-xl">
+            <div
+                className="py-3 px-6 sm:py-3 sm:px-8 rounded-xl"
+                style={{
+                    background: `url(${waves})`,
+                    backgroundPosition: "left top",
+                    backgroundSize: "600px",
+                }}
+            >
+                <form onSubmit={handleSubmit}>
+                    {step === 1 && (
+                        <Step1
+                            name={name}
+                            handleNameChange={handleNameChange}
+                        />
                     )}
-                    {step < stepLength && (
-                        <button type="button" onClick={nextStep} disabled={(step == 1 && name.trim() == "") || (step == 5 && selectedCategories.length <= 0)}>
-                            <img src={enabled} alt="Next" className="h-8 w-8" />
-                        </button>
+                    {step === 2 && (
+                        <Step2
+                            name={name}
+                            selectedTimePeriod={selectedTimePeriod}
+                            setSelectedTimePeriod={setSelectedTimePeriod}
+                        />
                     )}
-                    {step === stepLength && (
-                        <button type="submit" disabled={(step == 1 && name.trim() == "") || (step == 5 && selectedCategories.length <= 0)}>
-                            <img src={enabled} alt="Submit" className="h-8 w-8" />
-                        </button>
+                    {step === 3 && (
+                        <Step3
+                            name={name}
+                            selectedTimePeriod={selectedTimePeriod}
+                            goal={goal}
+                            setGoal={setGoal}
+                        />
                     )}
-                </div>
-            </form>
+                    {step === 4 && (
+                        <Step4
+                            name={name}
+                            goal={goal}
+                            select={select}
+                            setSelect={setSelect}
+                        />
+                    )}
+                    {/* {step === 5 && select === 'choose for me' && <CategoryStep name={name} selectedCategories={selectedCategories} setSelectedCategories={setSelectedCategories} handleCategoryClick={handleCategoryClick} />} */}
+                    <div
+                        className={`button-container flex w-full ${
+                            step > 1 ? "justify-between" : "justify-end"
+                        }`}
+                    >
+                        {step > 1 && (
+                            <button type="button" onClick={prevStep}>
+                                <img
+                                    src={disabled}
+                                    alt="Previous"
+                                    className="h-8 w-8"
+                                />
+                            </button>
+                        )}
+                        {step < stepLength && (
+                            <button
+                                type="button"
+                                onClick={nextStep}
+                                disabled={
+                                    (step == 1 && name.trim() == "") ||
+                                    (step == 5 &&
+                                        selectedCategories.length <= 0)
+                                }
+                            >
+                                <img
+                                    src={enabled}
+                                    alt="Next"
+                                    className="h-8 w-8"
+                                />
+                            </button>
+                        )}
+                        {step === stepLength && (
+                            <button
+                                type="submit"
+                                disabled={
+                                    (step == 1 && name.trim() == "") ||
+                                    (step == 5 &&
+                                        selectedCategories.length <= 0)
+                                }
+                            >
+                                <img
+                                    src={enabled}
+                                    alt="Submit"
+                                    className="h-8 w-8"
+                                />
+                            </button>
+                        )}
+                    </div>
+                </form>
+            </div>
         </div>
-        
     );
-}
+};
 
 export default ReadingListForm;
--- client/src/components/ReadingList/Shelf.jsx
+++ client/src/components/ReadingList/Shelf.jsx
@@ -1,19 +1,18 @@
-import React, { useEffect, useState, useRef, forwardRef } from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import CircularProgress from '@mui/material/CircularProgress';
-import waves from '../../assets/backgrounds/waves.png';
-import linear from '../../assets/backgrounds/linear.png';
+import React, { useEffect, useState, useRef, forwardRef } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import CircularProgress from "@mui/material/CircularProgress";
+import waves from "../../assets/backgrounds/waves.png";
 import enabled from "../../assets/buttons/enabled.svg";
 import disabled from "../../assets/buttons/disabled.svg";
 import remove from "../../assets/buttons/Remove.svg";
-import { useReadingList } from '../../context/ReadingListContext';
-import { getRandomBooks } from '../../utils/api';
-import CategoryStep from './StepperContent/CategoryStep';
-import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import PDFModal from './PDF/PDFModal';
+import { useReadingList } from "../../context/ReadingListContext";
+import { getRandomBooks } from "../../utils/api";
+import CategoryStep from "./StepperContent/CategoryStep";
+import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import PDFModal from "./PDF/PDFModal";
 
 const Shelf = forwardRef((props, ref) => {
     const {
@@ -26,7 +25,6 @@ const Shelf = forwardRef((props, ref) => {
         setSelectedCategoriesValue,
         updateBooksValue,
     } = useReadingList();
-
 
     const sliderRef = useRef(null);
     const [isLoading, setIsLoading] = useState(false);
@@ -54,14 +52,17 @@ const Shelf = forwardRef((props, ref) => {
             setWindowSize(window.innerWidth);
         };
         handleResize();
-        window.addEventListener('resize', handleResize);
+        window.addEventListener("resize", handleResize);
         return () => {
-            window.removeEventListener('resize', handleResize);
+            window.removeEventListener("resize", handleResize);
         };
     }, []);
 
     useEffect(() => {
-        if (selectedCategories.length && selectedCategories !== prevSelectedCategories.current) {
+        if (
+            selectedCategories.length &&
+            selectedCategories !== prevSelectedCategories.current
+        ) {
             setIsLoading(true);
             getRandomBooks(selectedCategories, goal, [])
                 .then((randomBooks) => {
@@ -90,13 +91,18 @@ const Shelf = forwardRef((props, ref) => {
     };
 
     const CustomNextArrow = ({ currentSlide }) => {
-        const itemsToShow = settings.responsive.find((item) => windowSize >= item.breakpoint)?.settings.slidesToShow || 0;
-        const isEnd = currentSlide >= (books.length / settings.rows) - itemsToShow - 1;
-    
+        const itemsToShow =
+            settings.responsive.find((item) => windowSize >= item.breakpoint)
+                ?.settings.slidesToShow || 0;
+        const isEnd =
+            currentSlide >= books.length / settings.rows - itemsToShow - 1;
+
         return (
             <img
                 src={enabled}
-                className={`h-8 w-8 absolute top-1/2 transform -translate-y-1/2 right-[-20px] cursor-pointer ${isEnd ? 'opacity-10 pointer-events-none' : ''}`}
+                className={`h-8 w-8 absolute top-1/2 transform -translate-y-1/2 right-[-20px] cursor-pointer ${
+                    isEnd ? "opacity-10 pointer-events-none" : ""
+                }`}
                 alt="Next"
                 onClick={() => {
                     if (!isEnd && sliderRef.current) {
@@ -106,13 +112,15 @@ const Shelf = forwardRef((props, ref) => {
             />
         );
     };
-    
+
     const CustomPrevArrow = ({ currentSlide }) => {
         const isStart = currentSlide === 0;
         return (
             <img
                 src={disabled}
-                className={`h-8 w-8 absolute top-1/2 transform -translate-y-1/2 left-[-20px] cursor-pointer ${isStart ? 'opacity-10 pointer-events-none' : ''}`}
+                className={`h-8 w-8 absolute top-1/2 transform -translate-y-1/2 left-[-20px] cursor-pointer ${
+                    isStart ? "opacity-10 pointer-events-none" : ""
+                }`}
                 alt="Previous"
                 onClick={() => {
                     if (!isStart && sliderRef.current) {
@@ -125,14 +133,10 @@ const Shelf = forwardRef((props, ref) => {
 
     let slidesToShow = 6;
 
-    if (windowSize <= 1100)
-        slidesToShow = 5;
-    if (windowSize <= 950)
-        slidesToShow = 4;
-    if (windowSize <= 800)
-        slidesToShow = 3;
-    if (windowSize <= 600)
-        slidesToShow = 2;
+    if (windowSize <= 1100) slidesToShow = 5;
+    if (windowSize <= 950) slidesToShow = 4;
+    if (windowSize <= 800) slidesToShow = 3;
+    if (windowSize <= 600) slidesToShow = 2;
 
     const maxRows = 2;
     const calculatedRows = Math.ceil(books.length / slidesToShow);
@@ -153,83 +157,120 @@ const Shelf = forwardRef((props, ref) => {
                 settings: {
                     slidesToShow: 5,
                     slidesToScroll: 5,
-                }
+                },
             },
             {
                 breakpoint: 950,
                 settings: {
                     slidesToShow: 4,
                     slidesToScroll: 4,
-                }
+                },
             },
             {
                 breakpoint: 800,
                 settings: {
                     slidesToShow: 3,
                     slidesToScroll: 3,
-                }
+                },
             },
             {
                 breakpoint: 600,
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 2,
-                }
+                },
             },
-        ]
+        ],
     };
 
-    const shareUrl = 'https://great-read.com';
+    const shareUrl = "https://great-read.com";
     const plural = books.length > 1 ? `${books.length} books` : `1 book`;
-    const postDescription = `I made a reading goal of ${plural} for ${period} on Great Read!\nCreate yours at: ${shareUrl}\n` + 
-        "Find books categorised as Procrastination Killers, Finding Yourself and Mind Tabs."
-    const linkedInShareUrl = `https://www.linkedin.com/feed/?shareActive=true&text=${encodeURIComponent(postDescription)}`;
+    const postDescription =
+        `I made a reading goal of ${plural} for ${period} on Great Read!\nCreate yours at: ${shareUrl}\n` +
+        "Find books categorised as Procrastination Killers, Finding Yourself and Mind Tabs.";
+    const linkedInShareUrl = `https://www.linkedin.com/feed/?shareActive=true&text=${encodeURIComponent(
+        postDescription
+    )}`;
     return (
-        <div className="bg-footer py-3 px-6 sm:py-0 sm:px-8 rounded-xl w-full max-w-[680px]" 
-            style={{
-                background: `linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url(${linear}), url(${waves})`,
-                backgroundSize: 'cover, auto 100%, cover',
-                backgroundPosition: 'center, right, center',
-                backgroundRepeat: 'no-repeat',
-            }}
-        >
-            <div className="flex flex-col items-center gap-4 sm:gap-4 py-4 sm:py-8">
-                <p className="manrope-semibold text-center text-xl sm:text-2xl text-black">{name}'s Reading List</p>
-                <div>
-                    <p className="manrope-semibold text-center text-lg sm:text-xl text-primary">{books.length} out of {goal === 1 ? "1 book" : `${goal} books`}</p>
-                    <p className="manrope-semibold text-center text-lg sm:text-xl text-primary">for {period}</p>
-                </div>
-                {selectionChoice == 'choose for me' && (
-                    <CategoryStep selectedCategories={selectedCategories} handleCategoryClick={handleCategoryClick} />
-                )}
-                <div className="w-full flex justify-center items-center bg-footer border-2 border-gray-400 border-dashed rounded-xl min-h-[200px]">
-                    
-                    {isLoading ? (
-                        <CircularProgress sx={{ color: '#8D5E20' }} />
-                    ) : (
-                        books.length === 0 && selectionChoice == 'i will choose' ? (
-                            <p className="text-md sm:text-lg text-center manrope-regular text-gray-400">Select books from below to add to your shelf.</p>
-                        ) 
-                        : books.length === 0 && selectionChoice == 'choose for me' ? (
+        <div className="bg-footer w-full max-w-[680px] rounded-xl">
+            <div
+                className="py-3 px-6 sm:py-0 sm:px-8 rounded-xl"
+                style={{
+                    background: `url(${waves})`,
+                    backgroundPosition: "left top",
+                    backgroundSize: "600px",
+                }}
+            >
+                <div className="flex flex-col items-center gap-4 sm:gap-4 py-4 sm:py-8">
+                    <p className="manrope-semibold text-center text-xl sm:text-2xl text-black">
+                        {name}'s Reading List
+                    </p>
+                    <div>
+                        <p className="manrope-semibold text-center text-lg sm:text-xl text-primary">
+                            {books.length} out of{" "}
+                            {goal === 1 ? "1 book" : `${goal} books`}
+                        </p>
+                        <p className="manrope-semibold text-center text-lg sm:text-xl text-primary">
+                            for {period}
+                        </p>
+                    </div>
+                    {selectionChoice == "choose for me" && (
+                        <CategoryStep
+                            selectedCategories={selectedCategories}
+                            handleCategoryClick={handleCategoryClick}
+                        />
+                    )}
+                    <div className="w-full flex justify-center items-center bg-footer border-2 border-gray-400 border-dashed rounded-xl min-h-[200px]">
+                        {isLoading ? (
+                            <CircularProgress sx={{ color: "#8D5E20" }} />
+                        ) : books.length === 0 &&
+                          selectionChoice == "i will choose" ? (
+                            <p className="text-md sm:text-lg text-center manrope-regular text-gray-400">
+                                Select books from below to add to your shelf.
+                            </p>
+                        ) : books.length === 0 &&
+                          selectionChoice == "choose for me" ? (
                             <>
-                                <p className="text-md sm:text-lg text-center manrope-regular text-gray-400">Select your categories and we will stack your shelf.</p>
+                                <p className="text-md sm:text-lg text-center manrope-regular text-gray-400">
+                                    Select your categories and we will stack
+                                    your shelf.
+                                </p>
                             </>
                         ) : (
-                            <Slider {...settings} ref={sliderRef} className='w-[90%] lg:w-[600px] p-2'>
+                            <Slider
+                                {...settings}
+                                ref={sliderRef}
+                                className="w-[90%] lg:w-[600px] p-2"
+                            >
                                 {books.map((book) => (
-                                    <div key={book._id} className='flex'>
+                                    <div key={book._id} className="flex">
                                         <div className="flex justify-center h-[160px] overflow-hidden">
-                                            <div className='relative mt-[8px] mr-[8px]'>
-                                                <img 
+                                            <div className="relative mt-[8px] mr-[8px]">
+                                                <img
                                                     src={remove}
-                                                    className='h-4 w-4 absolute top-[-6px] right-[-6px] cursor-pointer'
+                                                    className="h-4 w-4 absolute top-[-6px] right-[-6px] cursor-pointer"
                                                     onClick={() => {
-                                                        updateBooksValue(books.filter((readingBook) => readingBook._id !== book._id));
+                                                        updateBooksValue(
+                                                            books.filter(
+                                                                (readingBook) =>
+                                                                    readingBook._id !==
+                                                                    book._id
+                                                            )
+                                                        );
                                                     }}
                                                 />
-                                                <img src={book.image} alt="" className="w-24 rounded-md shadow-md h-36 object-cover cursor-pointer" 
+                                                <img
+                                                    src={book.image}
+                                                    alt=""
+                                                    className="w-24 rounded-md shadow-md h-36 object-cover cursor-pointer"
                                                     onClick={() => {
-                                                        updateBooksValue(books.filter((readingBook) => readingBook._id !== book._id));
+                                                        updateBooksValue(
+                                                            books.filter(
+                                                                (readingBook) =>
+                                                                    readingBook._id !==
+                                                                    book._id
+                                                            )
+                                                        );
                                                     }}
                                                 />
                                             </div>
@@ -237,30 +278,40 @@ const Shelf = forwardRef((props, ref) => {
                                     </div>
                                 ))}
                             </Slider>
-                        )
-                    )}
-                </div>
-                <div className='flex flex-col justify-center items-center gap-3 w-full text-black text-xs'>
-                    <p className='text-sm sm:text-md text-center'>You will be able to download it as a PDF and/or share it on your LinkedIn.</p>
-                    <div className='flex gap-4'>
-                        <button
-                            onClick={handleOpenModal}
-                            className={`w-28 rounded-lg shadow-lg px-2 py-1 flex justify-center items-center gap-1 bg-white
-                            ${books.length === 0 ? ' cursor-not-allowed opacity-50' : 'opacity-100'}`}
-                            disabled={books.length === 0}
-                        >
-                            <PictureAsPdfIcon className='text-red-700' />
-                            Download
-                        </button>
-                        <a
-                            href={linkedInShareUrl}
-                            target='_blank'
-                            className={`w-28 rounded-lg shadow-lg px-2 py-1 flex justify-center items-center gap-1 bg-white opacity-100`}
-                        >
-                            <LinkedInIcon className='text-blue-700' />
-                            Share
-                        </a>
-                        <PDFModal isModalOpen={isModalOpen} handleCloseModal={handleCloseModal} />
+                        )}
+                    </div>
+                    <div className="flex flex-col justify-center items-center gap-3 w-full text-black text-xs">
+                        <p className="text-sm sm:text-md text-center">
+                            You will be able to download it as a PDF and/or
+                            share it on your LinkedIn.
+                        </p>
+                        <div className="flex gap-4">
+                            <button
+                                onClick={handleOpenModal}
+                                className={`w-28 rounded-lg shadow-lg px-2 py-1 flex justify-center items-center gap-1 bg-white
+                            ${
+                                books.length === 0
+                                    ? " cursor-not-allowed opacity-50"
+                                    : "opacity-100"
+                            }`}
+                                disabled={books.length === 0}
+                            >
+                                <PictureAsPdfIcon className="text-red-700" />
+                                Download
+                            </button>
+                            <a
+                                href={linkedInShareUrl}
+                                target="_blank"
+                                className={`w-28 rounded-lg shadow-lg px-2 py-1 flex justify-center items-center gap-1 bg-white opacity-100`}
+                            >
+                                <LinkedInIcon className="text-blue-700" />
+                                Share
+                            </a>
+                            <PDFModal
+                                isModalOpen={isModalOpen}
+                                handleCloseModal={handleCloseModal}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
